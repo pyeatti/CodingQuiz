fe2dea1882bb74f4ab0ce413041ba95dfcfbca82
Ian Pyeatt <pyeatt08@gmail.com>
2020-07-27 20:02:58 -0500

    added quiz questions, and a function to cycle through as the user guesses correctly.

---
+++ assets/script.js
@@ -0,0 +1,93 @@
+// QUESTIONS
+var codingQuestions = [
+  {
+    question:
+      "What is the element called that is used to describe the set of variables, objects, and functions you explicitly have access to?",
+    choices: ["Scope", "Output Level", "Range", "Restriction"],
+    answer: "Scope",
+  },
+
+  {
+    question:
+      "What is the element used and hidden in code that explains things and makes the content more readable?",
+    choices: ["Quotations", "Notes", "Comments", "Comparisons"],
+    answer: "Comments",
+  },
+  {
+    question: "Arrays in Javascript can be used to store ____.",
+    choices: ["Numbers", "Strings", "Booleans", "All of the above"],
+    answer: "All of the above",
+  },
+  {
+    question:
+      "What is considered to be the most popular programming language in the world?",
+    choices: ["HTML", "Javascript", "Internet Explorer", "CSS"],
+    answer: "Javascript",
+  },
+  {
+    question: "What tag is used to define an unordered list that is bulleted?",
+    choices: ["ul", "p", "li", "ol"],
+    answer: "ul",
+  },
+];
+
+// VARIABLES
+let question = document.getElementById("question");
+let choiceText = document.getElementsByClassName("choice-text");
+let incorrectAnswer = document.getElementById("incorrect");
+let startQuiz = document.getElementById("btn");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+questionIndex = 0;
+
+// this function displays the proper text in each page element as the user navigates through all of the questions
+function getNewQuestion() {
+  let currentQuestion = codingQuestions[questionIndex];
+  question.innerHTML = currentQuestion.question;
+  choiceA.innerHTML = currentQuestion.choices[0];
+  choiceB.innerHTML = currentQuestion.choices[1];
+  choiceC.innerHTML = currentQuestion.choices[2];
+  choiceD.innerHTML = currentQuestion.choices[3];
+}
+
+getNewQuestion();
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    const selectedChoice = e.target.innerHTML;
+    let currentQuestion = codingQuestions[questionIndex];
+    // console.log(currentQuestion.answer);
+    if (selectedChoice === currentQuestion.answer) {
+      questionIndex++;
+      //   secondsLeft++
+      getNewQuestion();
+    } else {
+      document.getElementById("incorrect").textContent = "";
+      var incorrectChoice = document.createElement("h6");
+      incorrectChoice.textContent = "Wrong!";
+      incorrectAnswer.appendChild(incorrectChoice);
+    }
+
+    // console.log(selectedChoice);
+  });
+});
+
+// generateQuizQuestion();
+var timeEl = document.querySelector(".time");
+var mainEl = document.getElementById("main");
+
+var secondsLeft = 76;
+
+function setTime() {
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft;
+
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+    }
+  }, 1000);
+}
+
+setTime();
